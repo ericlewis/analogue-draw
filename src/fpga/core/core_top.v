d5401e1b83a917d31945f50544186ba6d8427cde
Eric Lewis <ericlewis777@gmail.com>
2022-09-01 10:04:52 -0500

    fix

--- src/fpga/core/core_top.v
+++ src/fpga/core/core_top.v
@@ -484,12 +484,17 @@ assign video_hs = vidout_hs;
     reg [9:0]   square_x = 'd135;
     reg [9:0]   square_y = 'd95;
 	 
-	 reg [9:0]   og_square_x = 'd135;
+    reg [9:0]   og_square_x = 'd135;
     reg [9:0]   og_square_y = 'd95;
 
 always @(posedge video_vs or negedge reset_n) begin
-	 square_x <= square_x + 1;
-	 square_y <= square_y + 1;
+    if (reset_n) begin
+        square_x <= og_square_x;
+        square_y <= og_square_y;
+    end else begin
+        square_x <= square_x + 1
+        square_y <= square_y + 1
+    end
 end
 
 always @(posedge clk_core_12288 or negedge reset_n) begin
