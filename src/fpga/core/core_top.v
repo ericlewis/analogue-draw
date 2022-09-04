bf55e5c97ae7814b0b61709e20601a6a567cb70b
Eric Lewis <ericlewis777@gmail.com>
2022-09-04 10:02:49 -0500

    fix

--- src/fpga/core/core_top.v
+++ src/fpga/core/core_top.v
@@ -484,8 +484,8 @@ assign video_hs = vidout_hs;
     reg [9:0] square_horz_move = -'d1;
     reg [9:0] square_vert_move = 'd1;
 
-    wire square_vert_collide = square_y >= VID_V_ACTIVE - SQUARE_SIZE;
-    wire square_horiz_collide = square_x >= VID_H_ACTIVE - SQUARE_SIZE;
+    wire square_vert_collide = square_y >= VID_V_ACTIVE - VID_V_BPORCH - SQUARE_SIZE;
+    wire square_horiz_collide = square_x >= VID_H_ACTIVE - VID_H_BPORCH - SQUARE_SIZE;
 
 always @(posedge vidout_vs or negedge reset_n) begin
     if (~reset_n) begin
